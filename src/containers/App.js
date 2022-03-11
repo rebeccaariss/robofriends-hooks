1e1226b4cbfda2e7932a183dc8d6f977b3e0138f
Rebecca Ariss <rebeccamarionruth@gmail.com>
2022-03-11 12:45:06 -0500

    Initial modifications for hooks conversion

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+// Deleted Component import here (no longer needed with Hooks):
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
@@ -6,46 +7,50 @@ import ErrorBoundary from '../components/ErrorBoundary';
 // import { robots } from './robots'; // old method
 import './App.css';
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-    }
+// Changed to function rather than "class App extends Component". "Hooks don't work inside classes – they let you use React without classes":
+function App() {
+    // Version one class syntax:
+    // constructor() {
+    //     super()
+    //     this.state = {
+    //         robots: [],
+    //         searchfield: ''
+    //     }
+    // }
 
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(users => this.setState({ robots: users }));
-    }
+    // No such thing as componentDidMount inside of a function, so this is no longer needed (this came from the imported Component class in version one):
+    // componentDidMount() {
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //     .then(response => response.json())
+    //     .then(users => this.setState({ robots: users }));
+    // }
 
-    onSearchChange = (event) => {
-        // console.log(event.target.value);
+    // This becomes a variable in version two:
+    const onSearchChange = (event) => {
         this.setState({ searchfield: event.target.value })
-        // console.log(filteredRobots);
     }
 
-    render() {
-        const { robots, searchfield } = this.state;
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
-        })
-            return !robots.length ?
-            <h1>Loading</h1> :
-            (
-                <div className='tc'>
-                    <h1 className='f1'>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
-                    <Scroll>
-                        <ErrorBoundary>
-                            <CardList robots={filteredRobots}/>
-                        </ErrorBoundary>
-                    </Scroll>
-                </div>
-            );
-    }
+    // render no longer needed:
+    // render() {
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+    })
+    return !robots.length ?
+    <h1>Loading</h1> :
+    (
+        <div className='tc'>
+            <h1 className='f1'>RoboFriends</h1>
+            {/* No longer need "this" keyword with Hooks: */}
+            {/* <SearchBox searchChange={this.onSearchChange} /> */}
+            <SearchBox searchChange={onSearchChange} />
+            <Scroll>
+                <ErrorBoundary>
+                    <CardList robots={filteredRobots}/>
+                </ErrorBoundary>
+            </Scroll>
+        </div>
+    );
 }
 
 export default App;
